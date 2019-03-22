f6390a48ceb497dc5d887ae9dc177290fba15c8b
Shubham Jain <s.shubjain@gmail.com>
2019-03-23 03:02:18 +0530

    Added EXPO's Uni-Modules to android project

--- android/app/src/main/java/com/mzeroes/minze/MainApplication.java
+++ android/app/src/main/java/com/mzeroes/minze/MainApplication.java
@@ -11,6 +11,17 @@ import com.facebook.soloader.SoLoader;
 
 import io.invertase.firebase.RNFirebasePackage;
 // optional packages - add/remove as appropriate
+// EXPO
+import org.unimodules.adapters.react.ReactAdapterPackage;
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.Package;
+import org.unimodules.core.interfaces.SingletonModule;
+
+import expo.modules.constants.ConstantsPackage;
+import expo.modules.permissions.PermissionsPackage;
+import expo.modules.filesystem.FileSystemPackage;
+// EXPO
 import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -31,6 +42,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  // -- EXPO
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(Arrays.<Package>asList(
+     new ReactAdapterPackage(),
+     new ConstantsPackage(),
+     new PermissionsPackage(),
+     new FileSystemPackage()
+  ), Arrays.<SingletonModule>asList());
+  //-- EXPO
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -59,7 +78,9 @@ public class MainApplication extends Application implements ReactApplication {
         new RNFirebaseMessagingPackage(),
         new RNFirebaseNotificationsPackage(),
         new RNFirebasePerformancePackage(),
-        new RNFirebaseStoragePackage()
+        new RNFirebaseStoragePackage(),
+        // -- Expo --
+        new ModuleRegistryAdapter(mModuleRegistryProvider)
       );
     }
 
