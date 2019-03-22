a057d8512d787cb6b67e68c10457984f86d8ce59
Shubham Jain <s.shubjain@gmail.com>
2019-03-23 02:35:11 +0530

    Added TypeScript intigration

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import App from './App';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App/>).toJSON();
+  expect(rendered).toBeTruthy();
+});
