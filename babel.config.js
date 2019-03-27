61a6ef194fceb35f4da8bc12e4be967933777845
Shubham Jain <s.shubjain@gmail.com>
2019-03-27 21:04:30 +0530

    Added server submodules

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
+      'module:metro-react-native-babel-preset',
       'module:react-native-dotenv',
-      'module:metro-react-native-babel-preset'
     ],
     // plugins: [
     //   [
