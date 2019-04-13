0a5e8b1553a40196da859082751b8256a8bdd4ba
Shubham Jain <s.shubjain@gmail.com>
2019-04-14 01:26:03 +0530

    Updated packages and fixed build
    Updated RNFirebase to latest
    Updated React and react-native to latest

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,3 @@
-// eslint-disable-next-line func-names
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      'module:react-native-dotenv',
-      'module:metro-react-native-babel-preset'
-    ],
-    // plugins: [
-    //   [
-    //     'module-resolver',
-    //     {
-    //       root: ['.'],
-    //       alias: {
-    //         api: './api',
-    //         assets: './assets',
-    //         theme: './theme',
-    //         components: './components',
-    //         screens: './screens',
-    //         navigation: './navigation',
-    //         test: './test',
-    //         utils: './utils'
-    //       }
-    //     }
-    //   ]
-    // ]
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
 };
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: "react-native",
+  setupFilesAfterEnv: ["./__mocks__/mockFirebase.js"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
