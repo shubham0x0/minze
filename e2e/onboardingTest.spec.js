81a5435a665f386eff9fb5c31afdd215b0579d28
Shubham Jain <s.shubjain@gmail.com>
2019-09-18 23:27:01 +0530

    typo

--- e2e/onboardingTest.spec.js
+++ e2e/onboardingTest.spec.js
@@ -1,4 +1,4 @@
-describe('Example', () => {
+describe('SanityTest', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
@@ -7,7 +7,7 @@ describe('Example', () => {
   //   await expect(element(by.id('title'))).toBeVisible();
   // });
 
-  it('should show hello screen after tap', async () => {
+  it('should follow onboarding Screens', async () => {
     await element(by.id('nextButton')).tap();
     await element(by.id('nextButton')).tap();
     await element(by.id('doneButton')).tap();
@@ -15,9 +15,6 @@ describe('Example', () => {
     await element(by.id('submitButton')).tap();
     await expect(element(by.text('Submit'))).toBeVisible();
     await element(by.id('submitButton_1')).tap();
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    await element(by.id('Account')).;
-    await element(by.text('Logout')).tap();
   });
 
   // it('should show world screen after tap', async () => {
