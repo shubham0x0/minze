8b14debd66a1345c5240c6683d7ae5f78da9bf00
Shubham Jain <s.shubjain@gmail.com>
2019-10-27 19:29:02 +0530

    env vars

--- app/config/index.ts
+++ app/config/index.ts
@@ -4,8 +4,6 @@
 import keys from './keys';
 
 export default Object.freeze({
-  APP_ENV: 'development',
-  SERVER_ENV: 'development',
   APP_VERSION: require('../../package.json').version,
   APP_URL: 'https://minze-server.herokuapp.com',
   API_ENDPOINT: 'https://minze-server.herokuapp.com',
